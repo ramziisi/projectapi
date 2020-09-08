5209c728b950738875aba8e2b25dc037556109d1
ramziisi <ramziisi2020@gmail.com>
2020-09-08 15:38:22 +0100

    apia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data,setData]=useState([]);
+  useEffect(() => {
+    const fetchDAta=async()=>{
+      const res=await axios.get('https://jsonplaceholder.typicode.com/users')
+      setData(res.data)
+    }
+   
+  fetchDAta()}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   {data.map(el=><h4>{el.email}</h4>)}
     </div>
   );
 }
